bcab22b1cdf28d6ee2b89020dc27d9fe966eee9b
mswat5 <mswat14361@gmail.com>
2024-11-20 02:19:07 +0530

    auth done

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithNavigate from "./components/auth/Auth0ProviderWithNavigate.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY =
+  "pk_test_ZXF1aXBwZWQtYW1vZWJhLTQwLmNsZXJrLmFjY291bnRzLmRldiQ";
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk publishable key to the .env.local file");
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
         <App />
-      </Auth0ProviderWithNavigate>
+      </ClerkProvider>
     </Router>
   </StrictMode>
 );
