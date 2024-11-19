6ab1dfe5a2c3363813ebf109112505ea2f34cf35
mswat5 <mswat14361@gmail.com>
2024-11-19 13:54:08 +0530

    auth key

--- src/components/auth/Auth0ProviderWithNavigate.tsx
+++ src/components/auth/Auth0ProviderWithNavigate.tsx
@@ -10,7 +10,7 @@ const Auth0ProviderWithNavigate = ({
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const redirectUri = "https://ecom-two-lovat.vercel.app/";
 
   if (!domain || !clientId || !redirectUri) {
     console.error("unable to initialise auth", {
